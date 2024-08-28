a1ab185ef4198af95e3e28170ebd904858e6a8e4
NIwnla <sonnguyenvietbao@gmail.com>
2024-08-28 09:59:53 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? process.env.VITE_BASE_URL : '/',
+    base: process.env.VITE_BASE_URL,
     server: {
       port: 3000,
     },
